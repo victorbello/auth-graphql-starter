fa208b7ce9b91119c4fb77ce69315fb9e5bdcdb1
Victor Bello <Victor.Bello@ff.com>
2017-06-30 16:36:23 -0700

    Figuring out the current user

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+
+import query from '../queries/CurrentUser';
+
+type Props = {
+  user: Object,
+  isFetching: boolean
+}
 
 class Header extends React.Component {
+  props: Props;
+
   render() {
+    if(this.props.isFetching) { return <h3>Loading...</h3> }
     return (
       <div>
         Header
@@ -10,4 +21,13 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+const mapDataToProps = result => ({
+  user: result.data.user,
+  isFetching: result.data.loading
+});
+
+const withData = graphql(query, {
+  props: mapDataToProps
+});
+
+export default withData(Header);
