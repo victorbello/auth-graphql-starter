63e25bcf3305f1a2957b2b2133b9a9806c0eb2a3
Victor Bello <Victor.Bello@ff.com>
2017-07-05 14:36:12 -0700

    Login form

---
+++ client/components/LoginForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import AuthForm from './AuthForm';
+
+class LoginForm extends React.Component {
+  render() {
+    return (
+      <div>
+        Login!
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
